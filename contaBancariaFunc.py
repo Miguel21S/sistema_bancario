1d41045403e725f7f673e81c2fca4fe071998f00
Miguel21S <leaismiguelss@gmail.com>
2025-03-09 12:41:34 +0100

    Creación de varias funciones

--- contaBancariaFunc.py
+++ contaBancariaFunc.py
@@ -1,65 +1,74 @@
 
-menu = ''' 
-Digite uma das opções
-1 Deposito
-2 Sacar
-3 Extrato
-4 Sair
-'''
-print(menu)
-
-def conta_bancaria():
-    SAQUE_MAXIMO = 500
-    LIMITE_DE_SAQUES = 3
-    saldo = 0
-    count_saque = 0
-    extrato = ""
-    saque_total =0
+SAQUE_MAXIMO = 500
+LIMITE_DE_SAQUES = 3
     
+def deposito(saldo, extrato, depositar):
+    if depositar > 0:
+        saldo += depositar
+        extrato += f"\nDepósito: + R$ {depositar} "
+        print(f"Depósito realizado successo. saldo atual: R$ {saldo}")
+        
+    else:
+        print("Valor inválido para depósito.")
+    return saldo, extrato
+
+def levantamento(saldo, extrato, count_saque):
+    if count_saque < LIMITE_DE_SAQUES:
+        sacar = int(input("Digite a quantia a sacar: "))
+        
+        if sacar > saldo:
+            print("Não tem saldo suficiente para realizar a operação")          
+        elif sacar > SAQUE_MAXIMO:
+            print("Has superado valor máximo a sacar")
+            
+        else:
+            saldo -= sacar
+            count_saque+=1
+            extrato += f"\nSaque: - R$ {sacar} "
+            print(f"Saque realizado com sucesso. saldo atual: R$ {saldo}")
+            print(f"Tens direito a 3 saques diario. Número de saques: {count_saque}/3")
+            
+    else:
+        print("Você atingiu o limite máximo de saques diários (3).")
+        
+    return saldo, extrato, count_saque
+        
+def extrato_bancario(saldo, extrato):
+    print("\n" + " EXtracto Bancario ".center(50, "="))
+    print(extrato if extrato else "Nenhuma transação realizada.")
+    print(f"Saldo atual: R$ {saldo}")
+ 
+def conta_bancaria():
+    saldo = 0
+    extrato = ""
+    count_saque = 0
+    
+    menu = ''' 
+    Digite uma das opções
+    1 Deposito
+    2 Sacar
+    3 Extrato
+    4 Sair
+    '''
     while True:
+        print(menu)
         valor = int(input("Digite o número da operação: "))
+        
         if valor == 1:
             depositar = int(input("Digite a quantia a depositar: "))
-            if depositar > 0:
-                saldo += depositar
-                extrato += f"\nDepósito: + R$ {depositar} "
-                print(f"Depósito realizado successo. saldo atual: R$ {saldo}")
-            else:
-                    print("Valor inválido para depósito.")
-
+            saldo, extrato = deposito(saldo, extrato, depositar)
+            
         elif valor == 2:
-            if count_saque < LIMITE_DE_SAQUES:
-                sacar = int(input("Digite a quantia a sacar: "))
-
-                if sacar > saldo:
-                    print("Não tem saldo suficiente para realizar a operação")
-
-                elif sacar > SAQUE_MAXIMO:
-                    print("Has superado valor máximo a sacar")
-
-                elif sacar <= saldo:
-                    count_saque += 1
-                    saldo -= sacar
-                    extrato += f"\nSaque: - R$ {sacar} "
-                    print(f"Saque realizado com sucesso. saldo atual: R$ {saldo}")
-                    print(f"Tens direito a 3 saques diario. Número de saques: {count_saque}/3")
-                
-                else:
-                        print("Valor inválido para saque.")
-                 
-            else:
-                print("Você atingiu o limite máximo de saques diários (3).")
-
+            saldo, extrato, count_saque = levantamento(saldo, extrato, count_saque)
+            
         elif valor == 3:
-            print("\n=================== EXtracto Bancario ===================")
-            print(extrato if extrato else "Nenhuma transação realizada.")
-            print(f"Saldo atual: R$ {saldo}")
-
+            extrato_bancario(saldo, extrato)
+            
         elif valor == 4:
             print("Volta sempre!")
             break
-
         else:
             print("Opção inválido. Intenta de novo")
 
 conta_bancaria()
+
